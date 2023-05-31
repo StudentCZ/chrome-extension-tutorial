b1afbaeef42602c38dd0f43bc11852b7d1b75259
Chao Zhou <Czhou0k@gmail.com>
2023-05-30 21:10:24 -0400

    added event listener to grab url and changes in video

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+/* eslint-disable no-undef */
+chrome.tabs.onUpdated.addListener((tabId, id) => {
+  if (tab.url && tab.url.includes('youtube.com/watch')) {
+    const queryParameters = tab.url.split('?')[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: 'New',
+      videoId: urlParameters.get('v'),
+    });
+  }
+});
