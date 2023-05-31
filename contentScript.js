fab771de0eda7e6ed69fe9900c0b2723e532224f
Chao Zhou <Czhou0k@gmail.com>
2023-05-30 21:15:09 -0400

    added global variable currentVideo

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,14 @@
 /* eslint-disable no-undef */
 (() => {
   let youtubeLeftControls, youtubePlayer;
+  let currentVideo = '';
 
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { type, value, videoId } = obj;
+
+    if (type === 'New') {
+      currentVideo = videoId;
+      newVideoLoaded();
+    }
   });
 })();
